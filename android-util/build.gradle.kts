103839ecd2164fd67910d8fa0d29a414b3c58276
Philip Klostermeier <p.klostermeier.dev@gmail.com>
2022-06-13 16:02:04 +0200

    Updated build.gradle.kts for version 0.2.0

--- android-util/build.gradle.kts
+++ android-util/build.gradle.kts
@@ -67,7 +67,7 @@ afterEvaluate {
 
                 groupId = "com.github.philipk99"
                 artifactId = "android-util"
-                version = "0.1.0"
+                version = "0.2.0"
             }
         }
     }
